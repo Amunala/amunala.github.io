9dddd2af7ecf71da63d0bf687b59ad36468a9dd5
David Ngacha <david.ngacha@nathandigital.com>
2025-02-26 15:44:51 +0300

    create data/projects.js file

---
+++ data/projects.js
@@ -0,0 +1,26 @@
+const projects = [
+    {
+      id: 1,
+      title: "Project One",
+      description: "This is a short description of the project.",
+      imageUrl: "/images/project1.jpg", 
+      tags: ["React", "Next.js", "UI/UX"],
+    },
+    {
+      id: 2,
+      title: "Project Two",
+      description: "Another project showcasing amazing work.",
+      imageUrl: "/images/project2.jpg",
+      tags: ["Vue", "Tailwind", "Animations"],
+    },
+    {
+      id: 3,
+      title: "Project Three",
+      description: "Exploring the power of modern web technologies.",
+      imageUrl: "/images/project3.jpg",
+      tags: ["Node.js", "Express", "MongoDB"],
+    },
+  ];
+  
+  export default projects;
+  
